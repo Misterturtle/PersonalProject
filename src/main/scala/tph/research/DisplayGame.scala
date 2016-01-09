83d0ad2ddde3c27692e90a06d1a4cbeb1e257ca5
Rob Conaway <rob@conaway58.com>
2016-01-09 16:38:29 -0500

    added WindowedApplication

--- src/main/scala/tph/research/DisplayGame.scala
+++ src/main/scala/tph/research/DisplayGame.scala
@@ -10,7 +10,7 @@ object DisplayGame extends App {
   val system = ActorSystem("TwitchPlaysHearthstone")
   val reporter:ActorRef = system.actorOf(Props[Reporter], "reporter")
   val gameWatcher:ActorRef = system.actorOf(Props(new GameWatcher(system, "Trebor", reporter)), "GameWatcher")
-  val logFileReader = system.actorOf(Props(new LogFileReader(system, new File("/Users/rconaway/Library/Logs/Unity/Player.log"), gameWatcher)), "logFileReader")
+  val logFileReader = system.actorOf(Props(new LogFileReader(system, new File("""C:\Program Files (x86)\Hearthstone\Hearthstone_Data\output_log.txt"""), gameWatcher)), "logFileReader")
 
   logFileReader ! LogFileReader.START
 
--- src/main/scala/tph/research/DisplayLogs.scala
+++ src/main/scala/tph/research/DisplayLogs.scala
@@ -9,7 +9,7 @@ object DisplayLogs extends App {
 
   val system = ActorSystem("TwitchPlaysHearthstone")
   val reporter:ActorRef = system.actorOf(Props[Reporter], "reporter")
-  val logFileReader = system.actorOf(Props(new LogFileReader(system, new File("/Users/rconaway/Library/Logs/Unity/Player.log"), reporter)), "logFileReader")
+  val logFileReader = system.actorOf(Props(new LogFileReader(system, new File("""C:\Program Files (x86)\Hearthstone\Hearthstone_Data\output_log.txt"""), reporter)), "logFileReader")
   logFileReader ! LogFileReader.START
 
 }
